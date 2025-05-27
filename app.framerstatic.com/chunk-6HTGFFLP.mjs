b7f373e909e3d621bc1b03d7375ece285e09c99d
aniketdas <aniket.das@outlook.in>
2025-05-28 00:38:26 +0530

    updated portfolio to version v2.0

---
+++ app.framerstatic.com/chunk-6HTGFFLP.mjs
@@ -0,0 +1,2 @@
+var r="popoutButton_pvjhuxd",p="popoutButtonDisabled_pcpv5lz",i="popoutButtonLarge_p1awmd8";export{r as a,p as b,i as c};
+//# sourceMappingURL=https://app.framerstatic.com/chunk-6HTGFFLP.mjs.map
---
+++ app.framerstatic.com/chunk-BYNVF2XM.mjs
@@ -0,0 +1,2 @@
+import{b as r}from"https://app.framerstatic.com/chunk-PLC2VY55.mjs";r("editorbar");
+//# sourceMappingURL=https://app.framerstatic.com/chunk-BYNVF2XM.mjs.map
