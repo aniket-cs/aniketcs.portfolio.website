65fd1e3efe2be60c5e12dc270b4f3f84c9ed1a79
aniketdas <aniket.das@outlook.in>
2025-05-29 19:09:06 +0530

    update to v3.0

---
+++ app.framerstatic.com/chunk-LIUBO4UK.mjs
@@ -0,0 +1,2 @@
+import{b as r}from"https://app.framerstatic.com/chunk-CAUAYXS6.mjs";r("editorbar");
+//# sourceMappingURL=https://app.framerstatic.com/chunk-LIUBO4UK.mjs.map
---
+++ framerusercontent.com/sites/5Bq3vR9hvzbCRluNbC6JmU/chunk-FIG6HNMV.mjs
@@ -0,0 +1,2 @@
+function t(e,a){return{description:"Aniket, the Cloud DevOps & Security Professional from India \u{1F1EE}\u{1F1F3}",favicon:"https://framerusercontent.com/assets/ybzYbRMKhoB900LdEUxzB7QITI.png",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/Bxifw3gNF9XzHutc7NwGxgOTM5U.png",title:"Aniket's Portfolio"}}export{t as a};
+//# sourceMappingURL=chunk-FIG6HNMV.mjs.map
